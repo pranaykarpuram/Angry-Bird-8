337cbcfe1f02d4f12882606b28d7884849f9cb2f
pranaykarpuram <71708903+pranaykarpuram@users.noreply.github.com>
2020-12-01 18:56:48 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,41 @@
+class Bird extends BaseClass {
+  constructor(x, y) {
+    super(x, y, 50, 50);
+    this.image = loadImage("sprites/bird.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.trajectory = [];
+    this.Visiblity = 255;
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+
+    if (this.body.velocity.x > 10 && this.body.position.x > 200) {
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
+    }
+
+    if (this.body.speed < 1 && birdState === 0&&gameState ==="launched") {
+      push();
+      this.Visiblity = this.Visiblity - 5;
+      tint(255, this.Visiblity);
+      for (var i = 0; i < this.trajectory.length; i++) {
+        image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+      }
+      pop();
+
+      if (this.Visiblity < 0) {
+        birdState = 1;
+      }
+    }
+
+    else if (birdState === 0) {
+      for (var i = 0; i < this.trajectory.length; i++) {
+        image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+      }
+    }
+  }
+}
